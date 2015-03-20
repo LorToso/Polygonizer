e0994134d8c45470962f8900ddec2a2b565c44d8
LorToso <LorToso@gmail.com>
2015-03-20 13:56:01 +0100

    Added method to find the outter border of an image

--- src/Polygonizer.java
+++ src/Polygonizer.java
@@ -15,12 +15,51 @@ public class Polygonizer {
     public static Polygon CreatePolygonFromImage(BufferedImage inputImage, int pointCount)
     {
         double[] vectorAngles = generateVectorAngles(pointCount);
-        Point[] farestPointPositions = generateFarestPointPositions(vectorAngles);
-        return null;
+        Point[] farestPointPositions = generateFarestPointPositions(vectorAngles, new Point(inputImage.getMinX(), inputImage.getMinY()));
+
+
+        Polygon polygon = new Polygon();
+
+        for (Point farestPointPosition : farestPointPositions) {
+            MovePointTowardsMid(farestPointPosition, inputImage);
+            polygon.addPoint(farestPointPosition.x, farestPointPosition.y);
+        }
+        return polygon;
     }
 
-    private static Point[] generateFarestPointPositions(double[] vectorAngles) {
-        return null;
+    private static void MovePointTowardsMid(Point farestPointPosition, BufferedImage inputImage) {
+        Point imageCenter = new Point(inputImage.getWidth()/2, inputImage.getHeight()/2);
+
+        while(isTransparentAt(inputImage, farestPointPosition))
+        {
+            Point referenceX;
+            Point referenceY;
+
+            if(farestPointPosition.x > imageCenter.x)
+                referenceX = new Point(farestPointPosition.x-1, farestPointPosition.y);
+            else
+                referenceX = new Point(farestPointPosition.x+1, farestPointPosition.y);
+
+            if(farestPointPosition.y > imageCenter.y)
+                referenceY = new Point(farestPointPosition.x, farestPointPosition.y-1);
+            else
+                referenceY = new Point(farestPointPosition.x, farestPointPosition.y+1);
+
+            farestPointPosition = referenceX.distance(imageCenter) < referenceY.distance(imageCenter) ? referenceX : referenceY;
+
+        }
+    }
+
+    private static Point[] generateFarestPointPositions(double[] vectorAngles, Point imageSize) {
+        Point[] farestPoints = new Point[vectorAngles.length];
+        for(int i=0; i < farestPoints.length; i++)
+        {
+            double angle = vectorAngles[i];
+            farestPoints[i] = new Point();
+            farestPoints[i].x = (int) (Math.cos(angle)*imageSize.y/2);
+            farestPoints[i].y = (int) (Math.sin(angle)*imageSize.x/2);
+        }
+        return farestPoints;
     }
 
     private static double[] generateVectorAngles(int pointCount) {
@@ -32,5 +71,13 @@ public class Polygonizer {
             vectorAngles[i] = i*degreePerPoint;
         }
         return vectorAngles;
+    }
+    private static int getAlphaAt(BufferedImage image, int x, int y)
+    {
+        return (image.getRGB(x,y) & 0xFF000000) >> 24;
+    }
+    private static boolean isTransparentAt(BufferedImage image, Point point)
+    {
+        return getAlphaAt(image, point.x, point.y) > 0;
     }
 }
