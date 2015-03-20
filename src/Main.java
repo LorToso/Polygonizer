25966538ea5d344aa91c8320ee9bca3454e376a4
LorToso <LorToso@gmail.com>
2015-03-20 13:56:26 +0100

    Added small test program

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,30 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by lorenzo on 19.03.15.
  */
 public class Main {
     public static void main(String[] args) {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("test.png"));
+        } catch (IOException ignored) {
+        }
+
+        JFrame frame = new JFrame();
+        frame.getContentPane().setLayout(new FlowLayout());
+        frame.getContentPane().add(new JLabel(new ImageIcon(img)));
+        frame.pack();
+        frame.setVisible(true);
+
+        Polygon polygon = Polygonizer.CreatePolygonFromImage(img);
+
+
 
     }
 }
